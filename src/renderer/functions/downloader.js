7b5df47c0e79a9cbc3c7bd63628d2133ee198feb
Ragnar Van den Broeck <ragnar.van.den.broeck@gmail.com>
2020-04-02 19:40:53 +0200

    changed downloader system

--- src/renderer/functions/downloader.js
+++ src/renderer/functions/downloader.js
@@ -1,42 +1,43 @@
-const { Stream } = require('stream')
 const youtubedl = require('youtube-dl')
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
+// const { spawn } = require('child_process')
 
 const tmpdir = path.join(os.tmpdir(), 'jaguardownloader')
 
-function downloader (url, info, stream) {
-  let ext
-  if (info.convert) {
-    ext = 'mkv'
-  } else {
-    ext = info.ext
-  }
+function downloader (url, info, progressCallback) {
+  const ext = info.convert ? 'mkv' : info.ext
   console.log(url, tmpdir)
+
   const video = youtubedl(url, ['-f', info.format[0].id], { cwd: tmpdir })
 
   let size
   let pos = 0
-  video.on('info', function (info) {
+  video.on('info', (data) => {
     console.log('Download started')
-    console.log('filename: ' + info._filename)
-    console.log('size: ' + info.size)
-    size = info.size
+    size = data.size
     console.log(`${info.id}.${ext}`)
     video.pipe(fs.createWriteStream(`${info.id}_video.${ext}`))
   })
-  video.on('data', (chunk) => {
+
+  video.on('data', chunk => {
+    pos += chunk.length
     if (size) {
-      pos += chunk.length
       const perc = Math.round((pos / size) * 100)
-      stream.push({status: 'downloading', progress: perc})
+      progressCallback('downloading', perc)
     }
   })
+
   if (info.format.length === 2) {
     const audio = youtubedl(url, ['-f', info.format[0].id], {cwd: tmpdir})
-    audio.pipe(fs.createWriteStream(`${info.id}_audio.${ext}`))
+    audio.on('info', () => {
+      audio.pipe(fs.createWriteStream(`${info.id}_audio.${ext}`))
+    })
   }
+
+  // merging
+  // const cmd = spawn('ffmpeg', ['-i', 'video.mkv', '-i' ,'audio.mkv', 'merged.mkv']);
 }
 
 // function getPerc(frame, fps, duration) {
@@ -45,12 +46,9 @@ function downloader (url, info, stream) {
 
 if (require.main === module) {
   let info = JSON.parse(fs.readFileSync('setting.json').toString())
-  const stream = new Stream.Readable({
-    read () {},
-    objectMode: true
+  downloader('https://www.youtube.com/watch?v=3RYNThid23g', info, (status, progress) => {
+    console.log(status, progress)
   })
-  downloader('https://www.youtube.com/watch?v=3RYNThid23g', info, stream)
-  stream.on('readable', () => console.log(stream.read().progress))
 }
 
 // export { downloader }
